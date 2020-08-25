d7cf2c974cc5bc1e62b0252d9b08bde2b647e2df
Christopher Backhouse <bckhouse@fnal.gov>
2020-08-25 13:14:13 -0500

    Need to set up stan as well as stan_math

--- jenkins/jenkins_build.sh
+++ jenkins/jenkins_build.sh
@@ -9,6 +9,7 @@ then
     source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh || exit 1
     setup root v6_18_04d -q ${QUALIFIER}:py2 || exit 1
     setup stan_math v2_18_0 -q $QUALIFIER || exit 1
+    setup stan v2_18_0 -q $QUALIFIER || exit 1
     setup eigen v3_3_5 || exit 1
     setup boost v1_70_0 -q $QUALIFIER || exit 1
 else
@@ -16,6 +17,7 @@ else
     source /cvmfs/nova.opensciencegrid.org/externals/setup || exit 1
     setup root v6_16_00 -q $QUALIFIER || exit 1
     setup stan_math v2.18.0 -q $QUALIFIER || exit 1
+    setup stan v2.18.0 -q $QUALIFIER || exit 1
     setup eigen v3.3.5 || exit 1
     setup boost v1_66_0a -q $QUALIFIER || exit 1
 fi
@@ -24,8 +26,8 @@ setup cmake v3_14_3 || exit 1
 setup ninja v1_8_2 || exit 1
 
 export PRODUCTS=/cvmfs/nova.opensciencegrid.org/externals/:$PRODUCTS
-setup osclib v00.03 -q ${QUALIFIER}:stan
-setup srproxy v00.12 -q ${QUALIFIER}:py2
+setup osclib v00.03 -q ${QUALIFIER}:stan || exit 1
+setup srproxy v00.12 -q ${QUALIFIER}:py2 || exit 1
 
 make clean
 time make || exit 2
