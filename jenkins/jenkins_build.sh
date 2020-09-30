d2c8866a945fe8df9c38268b5a2cda17ca977a5a
Christopher Backhouse <bckhouse@fnal.gov>
2020-09-30 09:41:25 -0500

    stan_math did not need to be changed. And update to py3 verrsion of ifdhc

--- jenkins/jenkins_build.sh
+++ jenkins/jenkins_build.sh
@@ -9,12 +9,12 @@ source /cvmfs/nova.opensciencegrid.org/externals/setup || exit 1
 
 if [[ $QUALIFIER == *e19* ]]
 then
-    setup root v6_18_04d -q ${QUALIFIER} || exit 1
-    setup stan_math v2_18_0a -q $QUALIFIER || exit 1
+    setup root v6_18_04d -q $QUALIFIER || exit 1
+    setup stan_math v2_18_0 -q $QUALIFIER || exit 1
     setup stan v2_18_0a -q $QUALIFIER || exit 1
     setup eigen v3_3_5 || exit 1
     setup boost v1_70_0 -q $QUALIFIER || exit 1
-    setup ifdhc v2_5_7 -q ${QUALIFIER}:p2715a || exit 1
+    setup ifdhc v2_5_7 -q ${QUALIFIER}:p372 || exit 1
 else
     setup root v6_16_00 -q $QUALIFIER || exit 1
     setup stan_math v2.18.0 -q $QUALIFIER || exit 1
