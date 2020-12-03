0ef6732ff9d184d10060b74aa2cd6a05e8716fa5
Christopher Backhouse <bckhouse@fnal.gov>
2020-12-03 07:03:06 -0600

    Enable c7 builds

--- Makefile
+++ Makefile
@@ -3,7 +3,9 @@
 
 all:
 	if [ x$(CMAKE_BUILD_TYPE) == x ]; then printf "\n\n\nCMAKE_BUILD_TYPE not set. Defaulting to 'Release'\n\n\n"; CMAKE_BUILD_TYPE=Release; fi
-	if [ ! -e build ]; then mkdir -p build; cd build; cmake -GNinja .. -DCMAKE_BUILD_TYPE=$(CMAKE_BUILD_TYPE); fi
+	if [ x$(CMAKE_CXX_COMPILER) == x ]; then printf "\n\n\nCMAKE_CXX_COMPILER not set. Defaulting to 'g++'\n\n\n"; CMAKE_CXX_COMPILER=g++; fi
+
+	if [ ! -e build ]; then mkdir -p build; cd build; cmake -GNinja .. -DCMAKE_BUILD_TYPE=$(CMAKE_BUILD_TYPE) -DCMAKE_CXX_COMPILER=$(CMAKE_CXX_COMPILER); fi
 	ninja -C build -j`nproc --all` install
 
 clean:
--- jenkins/jenkins_build.sh
+++ jenkins/jenkins_build.sh
@@ -3,11 +3,15 @@
 set +ex
 env
 
-source /cvmfs/nova.opensciencegrid.org/externals/setup || exit 1
-# Alternate source of products
-# source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh || exit 1
+if [[ $QUALIFIER == *c7* ]]
+then
+    # NOvA doesn't seem to have all the c7 products
+    source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh || exit 1    
+else
+    source /cvmfs/nova.opensciencegrid.org/externals/setup || exit 1
+fi
 
-if [[ $QUALIFIER == *e19* ]]
+if [[ $QUALIFIER == *e19* || $QUALIFIER == *c7* ]]
 then
     setup root v6_18_04d -q $QUALIFIER || exit 1
     setup stan_math v2_18_0 -q $QUALIFIER || exit 1
@@ -15,6 +19,7 @@ then
     setup boost v1_70_0 -q $QUALIFIER || exit 1
     setup ifdhc v2_5_7 -q ${QUALIFIER}:p372 || exit 1
 else
+    # e17
     setup root v6_16_00 -q $QUALIFIER || exit 1
     setup stan_math v2.18.0 -q $QUALIFIER || exit 1
     setup eigen v3.3.5 || exit 1
@@ -27,9 +32,20 @@ setup ninja v1_8_2 || exit 1
 
 make clean
 
+FLAGS=''
+
 if [[ $QUALIFIER == *prof* ]]
 then
-    time make CMAKE_BUILD_TYPE=Release || exit 2
+    FLAGS=$FLAGS' CMAKE_BUILD_TYPE=Release' || exit 2
 else
-    time make CMAKE_BUILD_TYPE=Debug || exit 2
+    FLAGS=$FLAGS' CMAKE_BUILD_TYPE=Debug' || exit 2
 fi
+
+if [[ $QUALIFIER == *c7* ]]
+then
+    FLAGS=$FLAGS' CMAKE_CXX_COMPILER=clang++' || exit 2
+else
+    FLAGS=$FLAGS' CMAKE_CXX_COMPILER=g++' || exit 2
+fi
+
+time make $FLAGS || exit 2
