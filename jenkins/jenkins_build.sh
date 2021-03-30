d9b6d201ea59208cd4689cef2fe303c1cb43ca67
Chris Backhouse <cjbackhouse@gmail.com>
2021-03-30 18:36:46 +0100

    Update type_erasure branch to latest SBN versions.

--- jenkins/jenkins_build.sh
+++ jenkins/jenkins_build.sh
@@ -3,20 +3,20 @@
 set +ex
 env
 
-if [[ $QUALIFIER == *c7* ]]
+if [[ $QUALIFIER == *e19* || $QUALIFIER == *c7* ]]
 then
     # NOvA doesn't seem to have all the c7 products
-    source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh || exit 1    
+    source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh || exit 1
 else
     source /cvmfs/nova.opensciencegrid.org/externals/setup || exit 1
 fi
 
 if [[ $QUALIFIER == *e19* || $QUALIFIER == *c7* ]]
 then
-    setup root v6_18_04d -q $QUALIFIER || exit 1
+    setup root v6_22_06a -q ${QUALIFIER}:p383b || exit 1
     setup stan_math v2_18_0 -q $QUALIFIER || exit 1
-    setup eigen v3_3_5 || exit 1
-    setup boost v1_70_0 -q $QUALIFIER || exit 1
+    setup eigen v3_3_9a || exit 1
+    setup boost v1_73_0 -q $QUALIFIER || exit 1
     setup ifdhc v2_5_7 -q ${QUALIFIER}:p372 || exit 1
 else
     # e17
