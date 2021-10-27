885aab5b431072677f580d932c66cabb82ece12c
Christopher Backhouse <bckhouse@fnal.gov>
2021-10-27 09:23:00 -0500

    Update to n311 qualifier for new sbn dependency on nutools v3_11_05

--- jenkins/make_table.sh
+++ jenkins/make_table.sh
@@ -8,14 +8,14 @@ echo VERSION=$TAG
 echo
 echo
 
-for EXPT in n308 n309
+for EXPT in n308 n311
 do
     for OPT in debug prof
     do
         for COMPILER in e19 e20 c7
         do
             if [[ $EXPT == n308 && $COMPILER != e19 ]]; then continue; fi
-            if [[ $EXPT == n309 && $COMPILER == e19 ]]; then continue; fi
+            if [[ $EXPT == n311 && $COMPILER == e19 ]]; then continue; fi
 
             echo FLAVOR=ANY
             echo QUALIFIERS=\"${OPT}:${COMPILER}:${EXPT}\"
