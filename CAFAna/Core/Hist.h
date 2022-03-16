12aba052f604f82331de2c6988a399906761f435
CiaranH1997 <42472974+CiaranH1997@users.noreply.github.com>
2022-03-16 14:07:29 -0400

    Update Hist.h to account for case where fSqrtErrs = true.
    Sorry this has taken me so long to get back to! Is this more like what you wanted? I was not sure whether I should set the existing array fSumSq to the sqrt errors if fSqrtErrs=true, or if I should create a new array and return that. The function is quite long now as well, so would you prefer it if I put the implementation into the .cxx file instead?

--- CAFAna/Core/Hist.h
+++ CAFAna/Core/Hist.h
@@ -45,7 +45,15 @@ namespace ana
     bool HasStan() const {return fType == kDenseStan;}
     const Eigen::ArrayXd& GetEigen() const {assert(fType == kDense); return fData;}
     const Eigen::ArrayXstan& GetEigenStan() const {assert(fType == kDenseStan); return fDataStan;}
-    const Eigen::ArrayXd& GetEigenSqErrors() const {assert(fType == kDense); return fSumSq;}
+    const Eigen::ArrayXd& GetEigenSqErrors() const {
+      assert(fType == kDense);
+      if (fSqrtErrs) {
+        fSumSq = fData.sqrt();
+        return fSumSq;
+      } else {
+        return fSumSq;
+      }
+    }
 
     int GetNbinsX() const;
     double GetBinError(int i) const;
