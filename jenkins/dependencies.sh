a457a2fc1b90f9167cfb9ee5c3cb988c440b0124
Gavin Davies <gsdavies@fnal.gov>
2023-01-28 17:13:56 -0600

    v1_series: update ifdhc version to match nova's needs for nu bundle v3_13_02c, hence make n313 a qualifier addition.

--- jenkins/dependencies.sh
+++ jenkins/dependencies.sh
@@ -10,21 +10,23 @@ fi
 
 QUAL=$1
 
-if [[ $QUAL == *:n308* ]]; then NQUAL=n308; QUAL=${QUAL/:n308/}; fi
 if [[ $QUAL == *:n311* ]]; then NQUAL=n311; QUAL=${QUAL/:n311/}; fi
+if [[ $QUAL == *:n313* ]]; then NQUAL=n312; QUAL=${QUAL/:n313/}; fi
 
-if [[ $NQUAL == n308 ]]
+if [[ $NQUAL == n311 ]]
 then
-    # These are the current (Apr 2021) nova versions (nutools v3_08_00)
-    echo root v6_18_04d -q$QUAL
-    echo boost v1_70_0 -q$QUAL
-    echo ifdhc v2_5_7 -q${QUAL}:p372
-else
     # These are the current (October 2021) sbn versions (nutools v3_11_05)
     echo root v6_22_08d -q${QUAL}:p392
     echo boost v1_75_0 -q$QUAL
 
     echo ifdhc v2_6_4 -q${QUAL}:p392
+
+else
+    # These are the current (January 2023) nova versions (nutools v3_13_02c)
+    echo root v6_22_08d -q${QUAL}:p392
+    echo boost v1_75_0 -q$QUAL
+
+    echo ifdhc v2_6_9 -q${QUAL}:p392
 fi
 
 echo stan_math v4_0_1 -q$QUAL
