3de5b98b659bf63a4f00848d045458bf46e6b75e
Cullen Sullivan <cullenmsullivan@gmail.com>
2025-06-18 09:24:07 -0500

    Cleanup nested ifs in dependencies.sh

--- jenkins/dependencies.sh
+++ jenkins/dependencies.sh
@@ -9,23 +9,29 @@ then
 fi
 
 QUAL=$1
+WANTSTAN=x
 
 if [[ $QUAL == *:n311* ]]; then NQUAL=n311; QUAL=${QUAL/:n311/}; fi
 if [[ $QUAL == *:n319* ]]; then NQUAL=n319; QUAL=${QUAL/:n319/}; fi
 
-WANTSTAN=yes
-if [[ $QUAL == *:stanfree ]]
-then
-  WANTSTAN=no
-  QUAL=${QUAL/:stanfree/}
-else
-  if [[ $QUAL == *:stanthread ]]
-  then
-    QUAL=${QUAL/:stanthread/}
-  else
+case $QUAL in
+  *:stan)
+    WANTSTAN=yes
     QUAL=${QUAL/:stan/}
-  fi
-fi
+    ;;
+  *:stanthread)
+    WANTSTAN=yes
+    QUAL=${QUAL/:stanthread/}
+    ;;
+  *:stanfree)
+    WANTSTAN=no
+    QUAL=${QUAL/:stanfree/}
+    ;;
+  *)
+    echo Must specify either \*:stan or \*:stanfree or \*:stanthread in QUALIFIER option
+    exit 1
+    ;;
+esac
 
 if [[ $NQUAL == n311 ]]
 then
